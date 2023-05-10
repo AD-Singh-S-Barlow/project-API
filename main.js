78ad3698dfe7e20d00782a153d6ea302aa3d142c
AmrithDSingh <amrithdathsingh04@gmail.com>
2023-05-10 16:29:33 +0100

    deployable

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-sam 
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => res.send("Hallo, madafaka!"));
+app.listen(port, () => {
+    console.log("Server is running at http://localhost:3000");
+});
