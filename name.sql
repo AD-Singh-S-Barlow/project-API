2140ea51964dbc5e4407bf137137f901e17fe8a2
AmrithDSingh <amrithdathsingh04@gmail.com>
2023-05-10 14:54:20 +0100

    table

---
+++ name.sql
@@ -0,0 +1,4 @@
+-- CREATE TABLE kiraKills(age INTEGER NOT NULL PRIMARY KEY , firstName TEXT NOT NULL, lastNameTEXT NOT NULL); 
+
+INSERT INTO kiraKills(age,firstName,lastNameTEXT)
+VALUES (59,"Quandale","Dingle")
