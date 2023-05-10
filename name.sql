db16cdbd8e61937f86a3509f5170683faa3ac1ac
AmrithDSingh <amrithdathsingh04@gmail.com>
2023-05-10 14:58:40 +0100

    table

--- name.sql
+++ name.sql
@@ -1,4 +1,6 @@
 -- CREATE TABLE kiraKills(age INTEGER NOT NULL PRIMARY KEY , firstName TEXT NOT NULL, lastNameTEXT NOT NULL); 
 
-INSERT INTO kiraKills(age,firstName,lastNameTEXT)
-VALUES (59,"Quandale","Dingle")
+-- INSERT INTO kiraKills(age,firstName,lastNameTEXT)
+-- VALUES (59,"Quandale","Dingle")
+
+SELECT * FROM kiraKills;
