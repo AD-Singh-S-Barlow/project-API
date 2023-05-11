c5aa310ffe1a0d15fd242da015745432d0fb5748
AmrithDSingh <amrithdathsingh04@gmail.com>
2023-05-11 14:01:16 +0100

    error when trying to get database online, dont know how to fix it:(

--- main.js
+++ main.js
@@ -1,8 +1,28 @@
 const express = require("express");
+const mysql = require("mysql");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => res.send("Hallo, madafaka!"));
-app.listen(port, () => {
-    console.log("Server is running at http://localhost:3000");
+const connection = mysql.createConnection({
+  host: "localhost",
+  database: "nameList",
 });
+
+connection.connect((err) => {
+  if (err) throw err;
+  console.log("Connected to MySQL database!");
+});
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  connection.query("SELECT * FROM kiraKills", (err, results) => {
+    if (err) throw err;
+    res.render("index.ejs", { data: results });
+  });
+});
+
+app.listen(port, () => {
+  console.log("Server is running at http://localhost:3000");
+});
+
