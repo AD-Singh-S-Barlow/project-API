e7aba25756c45b61d414f7cd0942cde5ac46a049
Samuel Barlow <samuelbarlow11@gmail.com>
2023-05-17 11:05:24 +0100

    new

--- name.sql
+++ name.sql
@@ -3,5 +3,5 @@ CREATE TABLE kiraKills(age INTEGER NOT NULL PRIMARY KEY , firstName TEXT NOT NUL
 
 INSERT INTO kiraKills(age,firstName,lastNameTEXT)
 VALUES (59,"Quandale","Dingle")
->>>>>>> 2140ea51964dbc5e4407bf137137f901e17fe8a2
+
 SELECT * FROM kiraKills;
