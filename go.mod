7836140e8135beae454dd04a12b9504de21e803f
AmrithDSingh <amrithdathsingh04@gmail.com>
2023-05-26 09:49:16 +0100

    retry authentication

---
+++ go.mod
@@ -0,0 +1,14 @@
+module 01-Login
+
+go 1.16
+
+require (
+	github.com/coreos/go-oidc/v3 v3.1.0
+	github.com/gin-contrib/sessions v0.0.5
+	github.com/gin-gonic/gin v1.9.0
+	github.com/joho/godotenv v1.4.0
+	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d
+	gopkg.in/go-playground/assert.v1 v1.2.1 // indirect
+	gopkg.in/go-playground/validator.v9 v9.29.1 // indirect
+	gopkg.in/yaml.v2 v2.2.8 // indirect
+)
--- main.go
+++ main.go
@@ -1,68 +1,100 @@
-
 package main
+
 import (
-   "net/http"
-   "github.com/gin-gonic/gin"
+	"log"
+	"net/http"
+
+	"01-Login/platform/authenticator"
+	"01-Login/platform/router"
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
+
 type NameList struct {
-  NameF string  `json: "fName:"`
-  NameL string  `json: "lName:"`
-  Age  float32 `json: "Age:"`
+	NameF string  `json:"fName"`
+	NameL string  `json:"lName"`
+	Age   float32 `json:"Age"`
 }
+
 var names = []NameList{
-  {NameF: "Quandale", NameL: "Dingle the third", Age: 59},
-  {NameF: "James", NameL: "Howlet", Age: 134},
-  {NameF: "Bihan", NameL: "Tundra", Age: 38},
+	{NameF: "Quandale", NameL: "Dingle the third", Age: 59},
+	{NameF: "James", NameL: "Howlet", Age: 134},
+	{NameF: "Bihan", NameL: "Tundra", Age: 38},
 }
+
 func getNames(c *gin.Context) {
-  c.IndentedJSON(http.StatusOK, names)
+	c.IndentedJSON(http.StatusOK, names)
 }
+
 func handlerFunc(c *gin.Context) {
-  c.String(200, "My record of Kira's kills")
+	c.String(200, "My record of Kira's kills")
 }
-func main() {
-   router := gin.Default()
-  router.GET("/", handlerFunc)
-  router.GET("/names", getNames)
-  router.PUT("/names", putNames)
-  router.POST("/names", postNames)
-  router.DELETE("/names", deleteNames)
-  router.Run()
+
+func deleteNames(c *gin.Context) {
+	var deleteItem NameList
+	if err := c.ShouldBindJSON(&deleteItem); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	var remove bool
+	for i, d := range names {
+		if d.NameF == deleteItem.NameF && d.NameL == deleteItem.NameL && d.Age == deleteItem.Age {
+			names = append(names[:i], names[i+1:]...)
+			remove = true
+			break
+		}
+	}
+
+	if remove {
+		c.JSON(http.StatusOK, gin.H{"message": "Resource deleted successfully"})
+	} else {
+		c.JSON(http.StatusNotFound, gin.H{"message": "Data not found"})
+	}
 }
-func deleteNames(C *gin.Context) {
-   var deleteItem NameList
-   if err := C.ShouldBindJSON(&deleteItem); err != nil {
-     C.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-     return
-  }
-   var remove bool
-   for i, d := range names {
-     if d.NameF == deleteItem.NameF && d.NameL == deleteItem.NameL && d.Age == deleteItem.Age {
-       names = append(names[:i], names[i+1:]...)
-       remove = true
-       break
-    }
-  }
-   if remove {
-    C.JSON(http.StatusOK, gin.H{"message": "Resource deleted successfully"})
-  } else {
-    C.JSON(http.StatusNotFound, gin.H{"message": "Data not found"})
-  }
-}
+
 func putNames(c *gin.Context) {
-   var update NameList
-   if err := c.ShouldBindJSON(&update); err != nil {
-     c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-     return
-  }
-   names = append(names, update)
-  c.IndentedJSON(http.StatusCreated, update)
+	var update NameList
+	if err := c.ShouldBindJSON(&update); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	names = append(names, update)
+	c.IndentedJSON(http.StatusCreated, update)
 }
+
 func postNames(c *gin.Context) {
-   var newName NameList
-   if err := c.BindJSON(&newName); err != nil {
-     return
-  }
-   names = append(names, newName)
-  c.IndentedJSON(http.StatusCreated, newName)
+	var newName NameList
+	if err := c.BindJSON(&newName); err != nil {
+		return
+	}
+
+	names = append(names, newName)
+	c.IndentedJSON(http.StatusCreated, newName)
+}
+
+func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("Failed to load the env vars: %v", err)
+	}
+
+	auth, err := authenticator.New()
+	if err != nil {
+		log.Fatalf("Failed to initialize the authenticator: %v", err)
+	}
+
+	r := gin.Default()
+
+	// Routes
+	r.GET("/", handlerFunc)
+	r.GET("/names", getNames)
+	r.PUT("/names", putNames)
+	r.POST("/names", postNames)
+	r.DELETE("/names", deleteNames)
+
+	log.Print("Server listening on http://localhost:3000/")
+	if err := http.ListenAndServe("0.0.0.0:3000", r); err != nil {
+		log.Fatalf("There was an error with the http server: %v", err)
+	}
 }
